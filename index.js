96424946cfb829246b816fcbfb7162faf38cd37e
ShAkEeB <shakeeb@ShAkEeBs-MacBook-Pro.local>
2022-11-08 11:40:18 +0600

    data base connect

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 2100;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-//chake server :
+//check server :
 app.get('/', (req, res) => res.send("node is open"))
 
+//database :
+const uri = process.env.DB_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const Mongodb = () => {
+    try {
+        client.connect();
+        console.log('datbase Connected');
+    } catch (error) {
+        console.log(error.name, error.message)
+    }
+};
+Mongodb();
 
-app.listen(port, () => console.log(port, "port is openå"))
+app.listen(port, () => console.log(port, "port is open"))
